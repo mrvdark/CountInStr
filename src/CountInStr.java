cdb278bc21ef05afdf0340465515209252131163
mrvdark <m.r.v.leo.dark@gmail.com>
2018-10-31 15:44:25 +0700

    done

---
+++ src/CountInStr.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class CountInStr {
+
+    public static void main(String[] args) {
+
+        String s;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        s=sc.nextLine();
+
+        System.out.println("Enter a Char: ");
+        char c ='a';
+        int count=0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i)==c) count++;
+        }
+
+        System.out.println("Count is: "+count);
+    }
+
+}
